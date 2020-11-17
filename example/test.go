12636e1f382234b33ad15f2003c86c2388b4fac9
justin <justin_bj10065@casauto.com.cn>
2020-11-17 12:03:14 +0800

    [DEV]Add support for struct

--- example/test.go
+++ example/test.go
@@ -5,12 +5,19 @@ import (
 	"urlencode"
 )
 
+type Book struct {
+	Name string `urlencode:"book_name"`
+	Id   int
+}
+
 func main() {
 	data := map[string]interface{}{
-		"aa": 1,
-		"bb": []string{"1", "pp"},
-		"cc": []map[string]string{map[string]string{"hh": "mm"}},
-		"dd": "88",
-		"tt": map[string]string{"uu": "9", "kk": "88"}}
+		"int":    1,
+		"arr":    []string{"tom", "lucy"},
+		"string": "text",
+		"obj":    map[string]string{"uu": "9", "kk": "88"},
+		"objarr": []map[string]string{map[string]string{"name": "namevalue"}},
+		"struct": Book{Name: "test", Id: 1},
+	}
 	fmt.Println(urlencode.Encode(data))
 }
--- urlencode.go
+++ urlencode.go
@@ -12,19 +12,7 @@ func Encode(valueMap map[string]interface{}) string {
 	kv := make(map[string]string)
 	for key, value := range valueMap {
 		v := reflect.ValueOf(value)
-		kind := v.Kind()
-		switch kind {
-		case reflect.String:
-			kv[key] = value.(string)
-		case reflect.Int, reflect.Int8, reflect.Int32, reflect.Int64:
-			kv[key] = fmt.Sprint(value)
-		case reflect.Map:
-			appendFromMap(kv, key, v)
-		case reflect.Slice, reflect.Array:
-			appendFromArray(kv, key, v)
-		default:
-			data.Set(key, "")
-		}
+		appendKeyValue(kv, key, v)
 	}
 	for k, v := range kv {
 		data.Set(k, v)
@@ -32,45 +20,62 @@ func Encode(valueMap map[string]interface{}) string {
 	return data.Encode()
 }
 
-func appendFromMap(data map[string]string, keyPrefix string, m reflect.Value) map[string]string {
-	for _, key := range m.MapKeys() {
-		v := m.MapIndex(key)
-		kind := v.Kind()
-		newKey := keyPrefix + "[" + key.String() + "]"
-		switch kind {
-		case reflect.String:
-			data[newKey] = v.String()
-		case reflect.Int, reflect.Int8, reflect.Int32, reflect.Int64:
-			data[newKey] = fmt.Sprint(v.Int())
-		case reflect.Map:
-			appendFromMap(data, newKey, v)
-		case reflect.Slice, reflect.Array:
-			appendFromArray(data, newKey, v)
-		default:
-			data[newKey] = ""
+func appendKeyValue(data map[string]string, key string, value reflect.Value) map[string]string {
+	kind := value.Kind()
+	switch kind {
+	case reflect.String:
+		data[key] = value.String()
+	case reflect.Int, reflect.Int8, reflect.Int32, reflect.Int64:
+		data[key] = fmt.Sprint(value.Int())
+	case reflect.Map:
+		appendFromMap(data, key, value)
+	case reflect.Slice, reflect.Array:
+		appendFromArray(data, key, value)
+	case reflect.Struct:
+		appendFromStruct(data, key, value)
+	case reflect.Ptr:
+		pv := reflect.Indirect(value)
+		if pv.Kind() == reflect.Struct {
+			appendFromStruct(data, key, pv)
 		}
+	default:
+		data[key] = ""
 	}
 	return data
 }
 
-func appendFromArray(data map[string]string, keyPrefix string, m reflect.Value) map[string]string {
-	len := m.Len()
-	for i := 0; i < len; i++ {
-		v := m.Index(i)
-		kind := v.Kind()
+func appendFromMap(data map[string]string, keyPrefix string, m reflect.Value) map[string]string {
+	for _, key := range m.MapKeys() {
+		v := m.MapIndex(key)
+		newKey := keyPrefix + "[" + key.String() + "]"
+		appendKeyValue(data, newKey, v)
+	}
+	return data
+}
+
+func appendFromArray(data map[string]string, keyPrefix string, arr reflect.Value) map[string]string {
+	l := arr.Len()
+	for i := 0; i < l; i++ {
+		v := arr.Index(i)
 		newKey := keyPrefix + "[" + strconv.Itoa(i) + "]"
-		switch kind {
-		case reflect.String:
-			data[newKey] = v.String()
-		case reflect.Int, reflect.Int8, reflect.Int32, reflect.Int64:
-			data[newKey] = fmt.Sprint(v.Int())
-		case reflect.Map:
-			appendFromMap(data, newKey, v)
-		case reflect.Slice, reflect.Array:
-			appendFromArray(data, newKey, v)
-		default:
-			data[newKey] = ""
+		appendKeyValue(data, newKey, v)
+	}
+	return data
+}
+
+func appendFromStruct(data map[string]string, keyPrefix string, s reflect.Value) map[string]string {
+	// attrNum := s.NumField()
+	t := s.Type()
+	attrNum := t.NumField()
+	for i := 0; i < attrNum; i++ {
+		field := t.Field(i)
+		tagValue := field.Tag.Get("urlencode")
+		name := field.Name
+		if tagValue != "" {
+			name = tagValue
 		}
+		newKey := keyPrefix + "[" + name + "]"
+		appendKeyValue(data, newKey, s.Field(i))
 	}
 	return data
 }
